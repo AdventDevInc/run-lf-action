d393e69528b8d3379ed65c4a565a054806a479e1
Dave <dave@blakey.co>
2025-04-04 19:04:07 +0200

    prepping for release

--- test.js
+++
@@ -1,6 +0,0 @@
-import * as core from '@actions/core';
-import fetch from 'node-fetch';
-
-console.log('Testing LoadForge GitHub Action dependencies');
-console.log('Core version:', core.issueCommand ? 'Working' : 'Not working');
-console.log('Fetch version:', fetch.name === 'fetch' ? 'Working' : 'Not working');
